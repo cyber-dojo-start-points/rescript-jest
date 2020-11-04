dfe0900b8df0aa0078b4fb4a806864ee3083326a
Jon Jagger <jon@jaggersoft.com>
2020-11-04 16:01:34 +0000

    Used image where sandbox user has rights in /etc/rescript/ and do an ln instead of a cp

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,18 +1,19 @@
 set -e
 
 # npm packages are in /etc/rescript/
-# Rescript compiles some .re into .bs.js files in this dir.
-# So we have to create a copy where we have permissions.
-cp -R /etc/rescript/node_modules ${CYBER_DOJO_SANDBOX}/node_modules
+# The [npm run build] command transpiles files in this dir
+# so the current user (sandbox) must have write rights there.
+
+ln -s /etc/rescript/node_modules ${CYBER_DOJO_SANDBOX}/node_modules
 
 # --------------------------------------------------------------
 function cyber_dojo_exit()
 {
-  # Text files under /sandbox are automatically returned...
+  # Text files under /sandbox are automatically returned to the browser.
   # So remove text files we don't want returned.
-  # From cp command above
+  # 1) From cp command above
   rm -rf ${CYBER_DOJO_SANDBOX}/node_modules
-  # Generated files...
+  # 2) Generated files...
   rm -rf ${CYBER_DOJO_SANDBOX}/lib
   rm ${CYBER_DOJO_SANDBOX}/.merlin
   find ${CYBER_DOJO_SANDBOX}/src -maxdepth 10 -type f -name "*.bs.js" -delete
@@ -20,5 +21,6 @@ function cyber_dojo_exit()
 trap cyber_dojo_exit EXIT SIGTERM
 
 # --------------------------------------------------------------
+
 npm run build
 npm run test
