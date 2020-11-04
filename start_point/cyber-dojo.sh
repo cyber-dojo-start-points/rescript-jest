a3d684b32668d55830d471a7963f12a00970a7fc
Jon Jagger <jon@jaggersoft.com>
2020-11-04 13:49:20 +0000

    Tidy cyber-dojo.sh

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,12 +1,15 @@
-# npm packages are in /etc/rescript/
-# Rescript compiles .re into .bs.js files in this dir.
-# So we have to create a copy where we have permissions.
+set -e
 
+# npm packages are in /etc/rescript/
+# Rescript compiles some .re into .bs.js files in this dir.
+# So we have to create a copy where we have permissions.
 cp -R /etc/rescript/node_modules ${CYBER_DOJO_SANDBOX}/node_modules
 
+# --------------------------------------------------------------
 function cyber_dojo_exit()
 {
-  # Ensure the symlink is removed.
+  # Text files under /sandbox are automatically returned...
+  # So remove text files we don't want returned.
   rm -rf ${CYBER_DOJO_SANDBOX}/node_modules
   rm -rf ${CYBER_DOJO_SANDBOX}/lib
   rm ${CYBER_DOJO_SANDBOX}/.merlin
@@ -14,5 +17,6 @@ function cyber_dojo_exit()
 }
 trap cyber_dojo_exit EXIT SIGTERM
 
+# --------------------------------------------------------------
 npm run build
 npm run test
